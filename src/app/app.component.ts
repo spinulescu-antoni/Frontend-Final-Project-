743c6ddeee94bb7a4f8a3c288e9dd2555a049ae5
spinulescu.antoni <toni07mich@gmail.com>
2024-04-18 21:48:45 +0300

    first step on frontend final project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemsGridComponent } from './items-grid/items-grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent,ItemsGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-FinalProject';
+
+
+//aici o sa facem request-uri catre backend
 }
